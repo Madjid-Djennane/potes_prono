508a61f35e4eb1b2b03f1ceb155077c072f89455
RAHAN RAHMANE <rahanrahmane@RAHANs-MacBook-Pro.local>
2020-11-26 22:09:05 +0100

    trauction avec i18next

--- src/components/noMatch/index.js
+++ src/components/noMatch/index.js
@@ -1,5 +1,7 @@
+
 import React from 'react'
 import { useLocation, Link } from 'react-router-dom'
+import i18n from './../../config/translations'
 
 const NoMatch = () => {
   let location = useLocation()
@@ -8,6 +10,8 @@ const NoMatch = () => {
       <h2>
         404 - <code>{location.pathname}</code> Not found
       </h2>
+  <p>{i18n.t('utilisateur')}</p>
+  <p>test</p>
       <Link to='/'>to Home</Link>
     </div>
   )
