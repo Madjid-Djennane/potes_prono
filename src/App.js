913ca120a52db851ad95777abbd64c9e4a8d7b53
RAHAN RAHMANE <rahanrahmane@RAHANs-MacBook-Pro.local>
2020-11-30 21:02:37 +0100

    test

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react'
 import './App.css'
 import Routes from './config/router'
 //import Header from '../src/components/header'
-
+//rany test
 import {Provider} from 'react-redux'
 import {store} from './config/store'
 
